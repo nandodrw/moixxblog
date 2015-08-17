3733523f934a465a719af899044498dd618c63f5
Fernando Catacora <nando.drw@gmail.com>
2015-08-17 18:19:11 -0500

    fix video url helper

--- content/themes/moixx/custom-helpers.js
+++ content/themes/moixx/custom-helpers.js
@@ -2,42 +2,49 @@ var hbs = require('express-hbs');
 
 
 function videoUrl(item, options) {
+  debugger;
+  if (!item || typeof(item) != 'object' || !item.length ) {
+    return new options.fn("");
+  };
 
-  // var tagString = "";
+  var tagString = "";
 
-  // var re = /\w+(?=\$)/g;
+  var re = /\w+(?=\$)/g;
 
-  // for (var i = item.length - 1; i >= 0; i--) {
-  //   tagString += item[i].name + ", "
-  // };
+  for (var i = item.length - 1; i >= 0; i--) {
+    tagString += item[i].name + ", "
+  };
 
-  // var resultRegx = re.exec(tagString)
+  var resultRegx = re.exec(tagString)
 
-  // if(resultRegx){
-
-  //   return new options.fn(resultRegx[0]);
-  // }
-  return new options.fn("");
+  if(resultRegx && resultRegx[0]){
+    return new hbs.SafeString(resultRegx[0]);
+  }
+  return new hbs.SafeString("");
 
 }
 
 function checkVideo(item, options) {
+  debugger;
+  if (!item || typeof(item) != 'object' || !item.length ) {
+    return options.inverse(this);
+  };
 
-  // var tagString = "";
+  var tagString = "";
 
-  // var re = /\w+(?=\$)/g;
+  var re = /\w+(?=\$)/g;
 
-  // for (var i = item.length - 1; i >= 0; i--) {
-  //   tagString += item[i].name + ", "
-  // };
+  for (var i = item.length - 1; i >= 0; i--) {
+    tagString += item[i].name + ", "
+  };
 
-  // var resultRegx = re.exec(tagString)
+  var resultRegx = re.exec(tagString)
 
-  // if(resultRegx && resultRegx[0]){
+  if(resultRegx && resultRegx[0]){
 
-  //   return options.fn(this);
+    return options.fn(this);
 
-  // }
+  }
   return options.inverse(this);
 
 }
