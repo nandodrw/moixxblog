caa5f0fa4b0c84a7a51ab0d201ebb07367717cd8
Fernando Catacora <nando.drw@gmail.com>
2015-09-09 15:45:44 -0500

    add margins on post content

--- content/themes/moixx/assets/js/functionality.js
+++ content/themes/moixx/assets/js/functionality.js
@@ -11,6 +11,14 @@
 
     });
 
+    $('#sub-list-nav').click(function(){
+
+      var url = 'http://corporaciontextil.us11.list-manage1.com/subscribe?u=640a49de720704dbcf6c86775&id=31a67406f5'
+
+      window.open(url, '_blank');
+
+    });
+
   });
 
 
